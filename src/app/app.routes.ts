c389f85588a5b3e987511decf155d740985ec594
MSr <ms26sr@gmail.com>
2025-10-16 16:26:31 +0330

    Initial App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,48 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'sign-in',
+    loadComponent: () =>
+      import('./pages/sign-in/sign-in.page').then((m) => m.SignInPage),
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () =>
+      import('./pages/sign-up/sign-up.page').then((m) => m.SignUpPage),
+  },
+  {
+    path: 'welcome',
+    loadComponent: () =>
+      import('./pages/welcome/welcome.page').then((m) => m.WelcomePage),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage),
+
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/home/home.page').then((m) => m.HomePage),
+      },
+      {
+        path: 'user-profile',
+        loadComponent: () =>
+          import('./pages/user-profile/user-profile.page').then(
+            (m) => m.UserProfilePage
+          ),
+      },
+    ],
   },
 ];

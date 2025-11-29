7f1e037f1c6e1c8b3fda1e54dced242713cefdc2
MSr <ms26sr@gmail.com>
2025-11-29 15:36:19 +0330

    Create page workflow

---
+++ src/app/pages/workflow/workflow.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WorkflowPage } from './workflow.page';
+
+describe('WorkflowPage', () => {
+  let component: WorkflowPage;
+  let fixture: ComponentFixture<WorkflowPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WorkflowPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
